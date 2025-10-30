45edd0d9c38b11c71037f5aa2ba9fe71490c0f94
nguyenducthanh1624 <uniemailorsth@gmail.com>
2025-10-30 13:58:30 +0700

    feat: add map manager

---
+++ MapManager.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class MapManager{
+    public MapManager() {
+        
+    }
+
+    // NUMBERS = DENSITY
+    // 5 = UNBREAKABLE
+    // 0 = DO NOTHING
+
+    public ArrayList<String> loadMapIntoArr(int currLevel) {
+        //System.out.println("gggggggggg");
+        ArrayList<String>arr = new ArrayList<String>();
+
+        File curLevelFile = new File("res/level"
+                + currLevel + ".txt");
+        //File curLevelFile = new File("level1.txt");
+
+        try (Scanner myScanner = new Scanner(curLevelFile)) {
+
+            while (myScanner.hasNextLine()) {
+                //String data = myScanner.nextLine();
+                arr.add(myScanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Can't find level" + currLevel + ".txt");
+            e.printStackTrace();
+        }
+
+        return arr;
+    }
+}
